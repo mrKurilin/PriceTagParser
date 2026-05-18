e891a538d3630ccc7d58bc6bbee22100783fda73
Anton Kurilin <anton.kurilin@lenta.com>
2026-05-18 21:13:26 +0300

    try open site faster

--- composeApp/src/webMain/kotlin/ru/pricetagparser/main.kt
+++ composeApp/src/webMain/kotlin/ru/pricetagparser/main.kt
@@ -3,9 +3,19 @@ package ru.pricetagparser
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 
+private const val COMPOSE_ROOT_ID = "composeAppRoot"
+
+/**
+ * Compose Web bootstrap.
+ *
+ * Тяжёлый `composeApp.js` подгружается лениво из `index.html` только после
+ * клика на «Старт», поэтому здесь нам нужно лишь смонтировать Compose в
+ * заранее подготовленный контейнер. Loader в `index.html` скрывается
+ * автоматически по MutationObserver, как только Compose добавит свои узлы.
+ */
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport {
+    ComposeViewport(viewportContainerId = COMPOSE_ROOT_ID) {
         App()
     }
 }
